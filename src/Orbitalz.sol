5585ff790450b9a91aced5f2275c92e533e3ba72
Jonathan Easterman <jonathaneasterman@gmail.com>
2022-06-07 16:57:48 +0100

    Add Orbitalz NFT contract + tests

---
+++ src/Orbitalz.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import {ERC721A} from "erc721a/ERC721A.sol";
+import {Owned} from "solmate/auth/Owned.sol";
+
+error BigBangNotStarted();
+error UniverseExpansionLimit();
+error RobotzCantMint();
+error BlackHoleError();
+
+contract Orbitalz is ERC721A, Owned {
+    uint256 public immutable walletLimit = 3;
+    uint256 public immutable orbitalzLimit = 10_000;
+    bool public afterBigBang = false;
+
+    constructor() ERC721A("Orbitalz", "ORBITALZ") Owned(msg.sender) {}
+
+    function bigBang() external payable {
+        if (!afterBigBang) revert BigBangNotStarted();
+        if (msg.sender != tx.origin) revert RobotzCantMint();
+        if (balanceOf(msg.sender) > 0) revert BlackHoleError();
+        uint256 toMint = min(orbitalzLimit - _totalMinted(), walletLimit);
+        if (toMint == 0) revert UniverseExpansionLimit();
+        _mint(msg.sender, toMint);
+    }
+
+    function godBigBang(address god, uint256 _orbitalz) public onlyOwner {
+        if (_orbitalz + _totalMinted() > orbitalzLimit) revert UniverseExpansionLimit();
+        _mint(god, _orbitalz);
+    }
+
+    function setAfterBigBang(bool _afterBigBang) external onlyOwner {
+        require(msg.sender == owner);
+        afterBigBang = _afterBigBang;
+    }
+
+    function harvestStarDust() public payable onlyOwner {
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success);
+    }
+
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+}
--- test/Orbitalz.t.sol
+++ test/Orbitalz.t.sol
@@ -1,22 +1,116 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+import {Test} from "forge-std/Test.sol";
+import {Orbitalz, BigBangNotStarted, UniverseExpansionLimit, RobotzCantMint, BlackHoleError} from "../src/Orbitalz.sol";
 
-import {SampleContract} from "../src/SampleContract.sol";
-
-contract SampleContractTest is DSTestPlus {
-    SampleContract sampleContract;
+contract OrbitalzTest is Test {
+    Orbitalz orbitalz;
 
     function setUp() public {
-        sampleContract = new SampleContract();
+        orbitalz = new Orbitalz();
     }
 
-    function testFunc1() public {
-        sampleContract.func1(1337);
+    function testName() public {
+        assertEq(orbitalz.name(), "Orbitalz");
     }
 
-    function testFunc2() public {
-        sampleContract.func2(1337);
+    function testSymbol() public {
+        assertEq(orbitalz.symbol(), "ORBITALZ");
+    }
+
+    function testOwner() public {
+        assertEq(orbitalz.owner(), address(this));
+    }
+
+    function testWalletLimit() public {
+        assertEq(orbitalz.walletLimit(), 3);
+    }
+
+    function testOrbitalzLimit() public {
+        assertEq(orbitalz.orbitalzLimit(), 10_000);
+    }
+
+    function testAfterBigBang() public {
+        assertEq(orbitalz.afterBigBang(), false);
+    }
+
+    function testBigBang() public {
+        orbitalz.setAfterBigBang(true);
+        address minter = vm.addr(1);
+        vm.prank(minter, minter);
+
+        orbitalz.bigBang();
+        assertEq(orbitalz.balanceOf(minter), 3);
+    }
+
+    function testCannotBigBangWithoutAfterBigBang() public {
+        vm.expectRevert(BigBangNotStarted.selector);
+        orbitalz.bigBang();
+    }
+
+    function testCannotBigBangIfMintLimit() public {
+        orbitalz.setAfterBigBang(true);
+
+        // mint 10_000 orbitalz
+        for (uint256 i = 0; i < 3_334; i++) {
+            // Force msg.sender == tx.origin
+            vm.prank(vm.addr(i + 1), vm.addr(i + 1));
+            orbitalz.bigBang();
+        }
+
+        assertEq(orbitalz.totalSupply(), 10_000);
+        assertEq(orbitalz.balanceOf(vm.addr(3_334)), 1);
+
+        // Force msg.sender == tx.origin
+        vm.prank(vm.addr(3_335), vm.addr(3_335));
+        vm.expectRevert(UniverseExpansionLimit.selector);
+        orbitalz.bigBang();
+        assertEq(orbitalz.balanceOf(vm.addr(3_335)), 0);
+        assertEq(orbitalz.totalSupply(), 10_000);
+    }
+
+    function testCannotBigBangIfContractMinted() public {
+        orbitalz.setAfterBigBang(true);
+        // msg.sender != tx.origin
+        vm.prank(vm.addr(12345), vm.addr(67890));
+
+        vm.expectRevert(RobotzCantMint.selector);
+        orbitalz.bigBang();
+    }
+
+    function testCannotBigBangMoreThanOncePerWallet() public {
+        orbitalz.setAfterBigBang(true);
+        // Force msg.sender == tx.origin
+        vm.startPrank(vm.addr(12345), vm.addr(12345));
+
+        orbitalz.bigBang();
+        vm.expectRevert(BlackHoleError.selector);
+        orbitalz.bigBang();
+        vm.stopPrank();
+    }
+
+    function testGodBigBang() public {
+        address mintTo = vm.addr(1);
+        vm.prank(orbitalz.owner());
+
+        orbitalz.godBigBang(mintTo, 1000);
+        assertEq(orbitalz.balanceOf(mintTo), 1000);
+    }
+
+    function testCannotGodBigBangIfNotOwner() public {
+        address minter = vm.addr(1);
+        vm.prank(minter);
+        vm.expectRevert("UNAUTHORIZED");
+        orbitalz.godBigBang(minter, 1000);
+        assertEq(orbitalz.balanceOf(minter), 0);
+    }
+
+    function testCannotGodBigBangMoreThanMaxSupply() public {
+        address minter = vm.addr(1);
+        vm.prank(orbitalz.owner(), orbitalz.owner());
+        vm.expectRevert(UniverseExpansionLimit.selector);
+        orbitalz.godBigBang(minter, 10_001);
+        assertEq(orbitalz.balanceOf(orbitalz.owner()), 0);
     }
 }
